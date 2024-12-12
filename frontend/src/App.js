7840db75ea67ab056499a1f44cfbd62d9bae2985
Betsy Alejandra <89090441+BetsyAlejandra@users.noreply.github.com>
2024-12-12 17:48:33 -0500

    lectura comoda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 // Componentes
 import FloatingButton from './Components/FloatingButton';
@@ -32,43 +32,52 @@ import BuscarPorFiltros from './pages/BuscarPorFiltros';
 import EditorsPage from './pages/EditorsPages';
 
 function App() {
+  const location = useLocation(); // Hook para obtener la ubicación actual
+
+  // Condición para ocultar Header y Footer
+  const hideHeaderFooter = location.pathname.startsWith('/read-chapter/');
+
+  return (
+    <div className="App">
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password-reset-request" element={<PasswordResetRequest />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/Novelas" element={<NovelsPage />} />
+        <Route path="/traductores" element={<TranslatorsPage />} />
+        <Route path="/editores" element={<EditorsPage />} />
+        <Route path="/postular" element={<Postulacion />} />
+        <Route path="/generos" element={<BuscarPorFiltros />} />
+        <Route path="/story-detail/:id" element={<StoryDetail />} />
+        <Route path="/read-chapter/:storyId/:chapterId" element={<ReadChapter />} />
+        <Route path="/add-chapter/:id" element={<AddChapter />} />
+
+        {/* Rutas protegidas */}
+        <Route path="/search-results" element={<SearchResults />} />
+        <Route path="/my-stories" element={<MyStories />} />
+        <Route path="/update/:id" element={<UpdateNovel />} />
+        <Route path="/edit-chapter/:storyId/:chapterId" element={<EditChapter />} />
+        <Route path="/upload" element={<NovelForm />} />
+        <Route path="/profile" element={<Perfil />} />
+        <Route path="/profileperson/:username" element={<PerfilUsuario />} />
+        <Route path="/admin-panel" element={<AdminPanel />} />
+      </Routes>
+      {!hideHeaderFooter && <FloatingButton />}
+      {!hideHeaderFooter && <Footer />}
+    </div>
+  );
+}
+
+function AppWrapper() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        {/* Definimos las rutas aquí */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/password-reset-request" element={<PasswordResetRequest />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/library" element={<Library />} />
-          <Route path='/Novelas' element={<NovelsPage />} />
-          <Route path='/traductores' element={<TranslatorsPage />} />
-          <Route path='/editores' element={<EditorsPage />} />
-          <Route path='/postular' element={<Postulacion />} />
-          <Route path='/generos' element={<BuscarPorFiltros />} />
-          <Route path="/story-detail/:id" element={<StoryDetail />} />
-          <Route path="/read-chapter/:storyId/:chapterId" element={<ReadChapter />} />
-          <Route path="/add-chapter/:id" element={<AddChapter />} />
-
-          {/* Rutas protegidas */}
-          <Route path='/search-results' element={<SearchResults />} />
-          <Route path="/my-stories" element={<MyStories />} />
-          <Route path="/update/:id" element={<UpdateNovel />} />
-          <Route path="/edit-chapter/:storyId/:chapterId" element={<EditChapter />} />
-          <Route path="/upload" element={<NovelForm />} />
-          <Route path="/profile" element={<Perfil />} />
-          <Route path="/profileperson/:username" element={<PerfilUsuario />} />
-          <Route path="/admin-panel" element={<AdminPanel />} />
-        </Routes>
-
-        <FloatingButton />
-        <Footer />
-      </div>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
