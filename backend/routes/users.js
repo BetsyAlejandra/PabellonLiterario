6773e501017afe9d63bc4b247d9ce17f88fd8a8b
Betsy Alejandra <89090441+BetsyAlejandra@users.noreply.github.com>
2025-02-26 23:23:10 -0500

    Correción importación

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -265,7 +265,7 @@ router.get('/profileperson/:username', async (req, res) => {
   }
 });
 
-router.get('/library', isAuthenticated, getSavedNovels);
+router.get('/library', isAuthenticated, userController.getSavedNovels);
 
 // En el archivo de rutas de usuarios
 router.get('/users/:id', async (req, res) => {
