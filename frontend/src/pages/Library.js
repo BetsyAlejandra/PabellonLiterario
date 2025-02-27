56c42c7cfa35ee48b334f82efe9fa702bc9a6c4b
Betsy Alejandra <89090441+BetsyAlejandra@users.noreply.github.com>
2025-02-27 00:13:03 -0500

    Ajuste visual

--- frontend/src/pages/Library.js
+++ frontend/src/pages/Library.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Library.css';
+import '../styles/Library.css'; // Asegúrate de que el archivo CSS esté correctamente importado.
 
 const Library = () => {
     const [library, setLibrary] = useState([]);
@@ -26,13 +26,17 @@ const Library = () => {
                 {library.length > 0 ? (
                     library.map((story) => (
                         <div key={story._id} className="library-card">
-                            <img src={story.coverImage} alt={story.title} className="library-card-image" />
+                            <img
+                                src={story.coverImage}
+                                alt={story.title}
+                                className="library-card-image"
+                            />
                             <div className="library-card-content">
                                 <h3 className="library-card-title">{story.title}</h3>
                                 <p className="library-card-description">{story.description}</p>
                                 <button
                                     className="library-card-button"
-                                    onClick={() => navigate(`/novels/${story._id}`)}
+                                    onClick={() => navigate(`/story-detail/${story._id}`)}
                                 >
                                     Ver detalles
                                 </button>
