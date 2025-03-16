3da36e4f83d43f0af7087086a315d94b98f0348f
Betsy Alejandra <89090441+BetsyAlejandra@users.noreply.github.com>
2025-03-15 19:53:35 -0500

    Ajuste anotaciones

--- frontend/src/pages/ReadChapter.js
+++ frontend/src/pages/ReadChapter.js
@@ -244,12 +244,22 @@ const ReadChapter = () => {
 
 
     const renderPopover = (annotation) => (
-
-        <Popover id={`popover-${popoverIdRef.current++}`} >
+        <Popover id={`popover-${popoverIdRef.current++}`} style={{ maxWidth: "300px" }}>
             <Popover.Header as="h3">Anotación</Popover.Header>
-            <Popover.Body>{annotation}</Popover.Body>
+            <Popover.Body style={{
+                maxWidth: "300px",
+                whiteSpace: "normal",
+                wordWrap: "break-word",
+                overflowWrap: "break-word",
+                fontSize: "14px",
+                lineHeight: "1.5",
+                padding: "10px"
+            }}>
+                {annotation}
+            </Popover.Body>
         </Popover>
     );
+
 
 
 
@@ -273,6 +283,7 @@ const ReadChapter = () => {
                         overlay={renderPopover(annotationText)}
                         container={chapterContainerRef.current}
                         rootClose
+                        containerPadding={10}
                     >
                         <span className="annotation"
                             style={{ cursor: 'pointer', color: '#2A2A2A', }}
